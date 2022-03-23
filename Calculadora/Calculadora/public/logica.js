04c8df0b354dd588c9fa26ae04d9e22b09801c49
MicaelaNoblia <micaelanoblia@gmail.com>
2022-03-23 14:34:57 -0300

    Escribe y borra numeros

--- Calculadora/Calculadora/public/logica.js
+++ Calculadora/Calculadora/public/logica.js
@@ -1,20 +1,59 @@
 window.onload = function(){ 
-    resultado=document.getElementById("resultado"); 
-    }
+    pantalla=document.getElementById("resultado"); 
+}
 
-a = "0"; //en a se almacena el numero en pantalla
-initNum = 1; //iniciar el numero en pantalla?: 1=si; 0=no (la opcion no es para continuar un numero que ya se estaba escribiendo)
+x = "0"; //en a se almacena el numero en pantalla
+xi = 1; //iniciar el numero en pantalla?: 1=si; 0=no (la opcion no es para continuar un numero que ya se estaba escribiendo)
 coma = 0; //estado coma decimal 0=no, 1=si
 
-function numero(aa) {
-    if (a=="0" || initNum==1  ) { //si no se ha ingresado otro num...
-        resultado.innerHTML=xx; //mostrar en la pantalla
-        a=aa; //guardar 
-        }
-     else { //si se esta continuando un num...
-        resultado.innerHTML+=xx; 
-        a+=aa; //se agrega y se guarda el valor
-        }
-     initNum=0 //dejamos la variable en cero porque ya esta iniciado el num
-}
+
+
+
+function numero(xx) { 
+    if (x=="0" || xi==1  ) {  //evalua que sea el primer numero en ingresarse
+       pantalla.innerHTML=xx; //muestra el num
+       x=xx; //iguala la variable
+       if (xx==".") { 
+          pantalla.innerHTML="0."; //si solo pone la coma entonces escribe 0.
+          x=xx; //guarda el valor
+          coma=1; //para recordar qur hay una coma puesta
+          }
+      }
+      else {
+          if (xx=="." && coma==0) { //si escribimos una coma por primera vez entonces escribe el num normal
+              pantalla.innerHTML+=xx;
+              x+=xx;
+              coma=1; 
+          }
+         
+          else if (xx=="." && coma==1) {} //si vuelve a poner una coma no hace nada
+          
+          else {
+              pantalla.innerHTML+=xx; //si ya estaba iniciado, escribir el numero, normal 
+              x+=xx
+          }
+       }
+       xi=0 //el numero ya esta iniciado
+    }
+
+    function operar(){
+
+
+    }
+
+    function resolver(){
+
+
+    }
+
+
+    function borrarTodo(){
+        pantalla.innerHTML = "0"
+
+    }
+
+
+    function porcentaje(){
+
+    }
 
