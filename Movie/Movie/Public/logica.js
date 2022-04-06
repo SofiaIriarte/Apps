9ac034f052c536214dca38e38b3b71092042ea8c
Sofia Iriarte <siriarte@correo.um.edu.uy>
2022-04-06 16:26:30 -0300

    json

--- Movie/Movie/Public/logica.js
+++ Movie/Movie/Public/logica.js
@@ -1,28 +1,5 @@
-window.onload = function(){ 
-    pantalla=document.getElementById("resultado"); 
-}
-
-function load() {
-	var mydata = JSON.parse(data);
-	document.body.innerHTML = mydata;
-}
-
 function showList() {
-	var settings = {
-		"async": true,
-		"crossDomain": true,
-		"url": "https://api.movie.com.uy/api/shows/rss/data",
-		"method": "GET"
-	  }
 	
-	$.ajax(settings).done(function (dataJson) {
-		console.log(dataJson);
-		console.log("Funciones: " + dataJson.contentCinemaShows.cinema);
-		var complejos = dataJson.contentCinemaShows.cinema;
-		$("#contentCinemaShowsCinema").append(complejos);
-		pantalla.innerHTML=content;
-		document.innerHTML(content);
-	  });
 
 }
 
