d443e34e2aceb845cc7704ff608b9b9105e22b26
Sofia Iriarte <siriarte@correo.um.edu.uy>
2022-04-20 18:38:14 -0300

    work

--- Movie/Movie/Public/logica.js
+++ Movie/Movie/Public/logica.js
@@ -1,7 +1,18 @@
-
-function showList() {
-	
-
+function getData(){
+    $.getJSON('https://api.movie.com.uy/api/shows/rss/data', function(data){
+            console.log(data);
+            var display = 
+                `User_ID: ${data.contentCinemaShows.movie}<br>
+                ID: ${data.contentCinemaShows.cinema}<br>
+                Title: ${data.contentCinemaShows.genre}`                
+            $(".display").html(display);
+    })
+    .done(function(data) {
+        console.log("Done");
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        throw 'Error';
+    });        
 }
 
 
