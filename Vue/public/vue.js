4589ed2526af2d9eb5262e5d214d52bb1755d444
Sofia Iriarte <siriarte@correo.um.edu.uy>
2022-04-20 21:28:07 -0300

    clase

--- Vue/public/vue.js
+++ Vue/public/vue.js
@@ -1,11 +1,18 @@
 window.onload=function(){
     
     let vm=new Vue({
-        el:'#vue-app',
+        el:'#app-4',
         data:{
-            message:'Hello Vue.js!'
-        }
+            perros:['isa', 'ameli', 'mozart', 'indio', 'pepe', 'pancho', 'lola', 'jambo']
+        },
+        methods: {
+            agregar: function () {
+              this.append(perroNuevo) 
+            }
+          }
     })
+    vm.plus()
+    vm.perros
 
     setTimeout(function(){
         vm.message='magia'
--- Vue/webserver.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-var app = express();
-var port = process.env.PORT || 3000;
-app.use(serveStatic(path.join(__dirname, 'public')));
-app.listen(port, function () {
-console.log('listening on port:', port);
-});
