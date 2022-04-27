799997f63d31c65691999967d0e28f89bf1907ff
Sofia Iriarte <siriarte@correo.um.edu.uy>
2022-04-27 16:13:31 -0300

    cambios

--- Movie/Movie/Public/logica.js
+++ Movie/Movie/Public/logica.js
@@ -1,19 +1,36 @@
 function getData(){
     $.getJSON('https://api.movie.com.uy/api/shows/rss/data', function(data){
+            var info=[];
             var display = 
-                `User_ID: ${data.contentCinemaShows.movie}<br>
-                ID: ${data.contentCinemaShows.cinema}<br>
-                Title: ${data.contentCinemaShows.genre}`                
-            $(".display").html(display);
+                data.contentCinemaShows.forEach(element => {
+                    movie: {[element].movie};                       // para que me sirven?????????????????????
+                    description:{[element].description};
+                    cinemaShows:{[element].cinemaShows};
+                    /*cinema:{[element].cinemaShows.cinema};
+                    shows:{[element].cinemaShows.shows};
+                    date:{[element].cinemaShows.shows.date};
+                    timeToDisplay:{[element].cinemaShows.shows.timeToDisplay};*/
+                    var movie = JSON.stringify(element.movie);
+                    //console.log(strJson);   
+                    //$(".display").html(strJson);
+                    info.push(movie);
+                    //html: info.join( "" )
+                    $( "<ul/>", {                     //donde abre el ol??????????????    <algo/>???????????????
+                        //class: "my-new-list",        //q hace???????????
+                        html: info.join()             //q hace???????????
+                    }).appendTo( "body" );
+                    })
+                               
+            /*$(".display").html(data.contentCinemaShows);
             var items = [];
             $.each( data, function( key, val ) {
                 items.push( "<li id='" + key + "'>" + val + "</li>" );
-            });
-            
+            })u
+            console.log(items);
             $( "<ul/>", {
                 "class": "my-new-list",
                 html: items.join( "" )
-            }).appendTo( "body" );
+            }).appendTo( "body" );*/
     })
     .done(function(data) {
         console.log("Done");
