0d0298cbdda4d9fdbdec4ce92ab2d810cd5e3359
Sofia Iriarte <siriarte@correo.um.edu.uy>
2022-05-01 22:38:22 -0300

    d

--- www/custom/custom.js
+++ www/custom/custom.js
@@ -284,11 +284,11 @@ function installEvents() {
 	});
 	
 	//Pure javascript
-	var menuOptions = document.getElementById('menuoptions')
+	/*var menuOptions = document.getElementById('menuoptions')
 	menuOptions.addEventListener('click', function() {
 		mui.alert('Sorry. Nothing to do!', "Attention");
 		return false;
-	}, false);
+	}, false);*/
 	
 }
 
@@ -329,83 +329,97 @@ window.onload=function(){
 	   },
 	   
 	   methods: {
-		   
+		function () {
+            var _this = this;
+            $.getJSON('https://api.movie.com.uy/api/shows/rss/data', function (data) {
+				var info = [];
+                var cines = [];
+                var peliculasComplejos = [];
+                var peliculas = [];
+                var arrSinDuplicacionesM=[];
+                
+                var display = 
+                    data.contentCinemaShows.forEach(element => { 
+                        
+                        var contenido=[];
+                        var movie=null;
+                        var description=null;
+                        var posterURL=null;
+                        var trailerURL=null;
+                        var shows=[];
+                
+                        var cine = [];
+                
+                        this.movie = element.movie;
+                        
+                        this.description = element.description;
+                        this.posterURL = element.posterURL;
+                        this.trailerURL = element.trailerURL;
+                        
+                        
+                        element.cinemaShows.forEach(cinemas =>{
+                            
+                            var location=[]; 
+                            location.push(cinemas.cinema);
+                            cine = cinemas.cinema;
+                            cinemas.shows.forEach(show => {
+                                var dateTime=[];
+                                dateTime.push(show.date);
+                                dateTime.push(show.timeToDisplay);
+                                location.push(dateTime); 
+    
+    
+                            })
+                            shows.push(location);
+                    
+                        })
+                        
+                        
+                        contenido.push(this.movie);
+                        contenido.push(this.description);
+                        contenido.push(this.posterURL);
+                        contenido.push(this.trailerURL);
+                        contenido.push(this.shows);
+
+                        
+                        peliculas.push(this.movie);
+                        let setM = new Set(peliculas.map(JSON.stringify))
+                        arrSinDuplicacionesM = Array.from(setM).map(JSON.parse);
+						var currentMovie=this.movie;
+						peliculasComplejos.push({cine,currentMovie});
+
+						cines.push(cine);
+                        
+                        info.push(element);  
+                        $("<ul/>", {
+                            html: info.join()
+                        });
+                    });
+        
+                
+                let set = new Set(cines.map(JSON.stringify))
+                let arrSinDuplicaciones = Array.from(set).map(JSON.parse);
+                
+				
+                        
+				let setC = new Set(this.peliculasComplejos.map(JSON.stringify))
+				arrSinDuplicacionesC = Array.from(setC).map(JSON.parse);
+				
+				console.log(arrSinDuplicacionesC);
+
+        
+                _this.cartelera = info;
+                _this.cines=arrSinDuplicaciones;
+				_this.peliculas.push(this.arrSinDuplicacionesM);
+				console.log(_this.peliculas);
+                
+    
+  
+            });
+
+      },
    
-		   function () {
-			   var _this = this;
-			   $.getJSON('https://api.movie.com.uy/api/shows/rss/data', function (data) {
-				   var info = [];
-				   var cines = [];
-				   var peliculas = [];
-				   var arrSinDuplicacionesM=[];
-				   
-				   var display = 
-					   data.contentCinemaShows.forEach(element => { 
-						   
-						   var contenido=[];
-						   var movie=null;
-						   var description=null;
-						   var posterURL=null;
-						   var trailerURL=null;
-						   var shows=[];
-				   
-						   var cine = [];
-				   
-						   this.movie = element.movie;
-						   
-						   this.description = element.description;
-						   this.posterURL = element.posterURL;
-						   this.trailerURL = element.trailerURL;
-						   
-						   
-						   element.cinemaShows.forEach(cinemas =>{
-							   var location=[]; 
-							   location.push(cinemas.cinema);
-							   cine = cinemas.cinema;
-							   cinemas.shows.forEach(show => {
-								   var dateTime=[];
-								   dateTime.push(show.date);
-								   dateTime.push(show.timeToDisplay);
-								   location.push(dateTime);
-	   
-	   
-							   })
-							   shows.push(location);
-							   
-					   
-						   })
-						   
-						   
-						   contenido.push(this.movie);
-						   contenido.push(this.description);
-						   contenido.push(this.posterURL);
-						   contenido.push(this.trailerURL);
-						   contenido.push(this.shows);
-   
-						   
-						   peliculas.push(this.movie);
-						   let setM = new Set(peliculas.map(JSON.stringify))
-						   arrSinDuplicacionesM = Array.from(setM).map(JSON.parse);
-						   cines.push(cine);
-						   
-						   info.push(element); 
-						   $("<ul/>", {
-							   html: info.join()
-						   });
-					   });
 		   
-				   
-				   let set = new Set(cines.map(JSON.stringify))
-				   let arrSinDuplicaciones = Array.from(set).map(JSON.parse);
-		   
-				   _this.cartelera = info;
-				   _this.cines=arrSinDuplicaciones;
-				   _this.peliculas.push(this.arrSinDuplicacionesM);
-	   
-			   });
-   
-		 },
-		 
 		 getCinemas(){
 		   console.log("entro");
 			   
@@ -413,7 +427,11 @@ window.onload=function(){
 			   html: cines.join()
 		   });
    
-		 }
+		 },
+		 onChange: function(event){
+			this.cineSeleccionado=event.target.value;
+			return event.target.value;
+		  }
    
 	   },
 	   created() {
